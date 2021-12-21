ce6eb7ff9b8ceeb28a198bd6e2b154603aeac70b
Sanjeev94 <sanjeev.dubey930@gmail.com>
2021-12-22 04:33:47 +0530

    adding axios
    Fetching the backend from Heroku and data from mongo db

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import Video from './Video'
+import Video from './Video';
+import axios from './axios';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await axios.get('/v2/posts');
+      setVideos(response.data);
+      return response;
+    }
+    fetchPosts();
+
+  }, []);
+
   return (
     // BEM
     <div className="app">
       
 
       <div className="app__videos">
-      <Video url="https://media.chingari.io/uploads/915e1320-3f90-4057-885b-3825954d8d4d-1638182113726/webpath_915e1320-3f90-4057-885b-3825954d8d4d-1638182113726.mp4"
+        {videos.map(({url, channel, description, song, likes, messages, shares}) => (
+          <Video 
+            url={url}
+            channel={channel}
+            description={description}
+            song={song}
+            likes={likes}
+            messages={messages}
+            shares={shares}
+
+          />
+        ))}
+      {/* <Video url="https://media.chingari.io/uploads/915e1320-3f90-4057-885b-3825954d8d4d-1638182113726/webpath_915e1320-3f90-4057-885b-3825954d8d4d-1638182113726.mp4"
       channel='abc'
       description='TIK TOK Using MERN STACK'
       song="99 problems but React aint one"
@@ -23,7 +48,7 @@ function App() {
       song="99 problems but React aint one"
       likes={236}
       messages={489}
-      shares={600} />
+      shares={600} /> */}
       </div>
     </div>
   );
